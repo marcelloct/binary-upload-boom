1fcb907befbc44db69e95d2ddab89dd8f8a8dba3
Marcello C <marcello_ct@hotmail.com>
2025-12-08 19:12:57 -0300

    Adding like and delete methods to comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,7 +1,10 @@
 const Comment = require("../models/Comment");
+const Post = require("../models/Post");
 
 module.exports = {
   createComment: async (req, res) => {
+    console.log(req.params);
+    console.log(req.body);
     try {
       await Comment.create({
         comment: req.body.comment,
@@ -13,6 +16,34 @@ module.exports = {
       res.redirect("/post/" + req.params.id);
     } catch (err) {
       console.log(err);
+    }
+  },
+  likeComment: async (req, res) => {
+    // Mongoose's populate() is a crucial feature for managing relationships between data in MongoDB, where data is often stored in a denormalized fashion.
+    // Simplifying, Get data from a relationship collection (ObjectId)
+    const p = await Comment.findById(req.params.id).populate("post");
+    try {
+      await Comment.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $inc: { likes: 1 },
+        }
+      );
+      console.log("Likes +1");
+      res.redirect(`/post/${p.post._id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  deleteComment: async (req, res) => {
+    const p = await Comment.findById(req.params.id).populate("post");
+    try {
+      // Delete post from db
+      await Comment.deleteOne({ _id: req.params.id });
+      console.log("Deleted Comment");
+      res.redirect(`/post/${p.post._id}`);
+    } catch (err) {
+      res.redirect(`/post/${p.post._id}`);
     }
   },
 };
