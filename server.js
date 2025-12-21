8519a12e9d3815c85fa5d48b17b62ab2b07aa069
Marcello C <marcello_ct@hotmail.com>
2025-12-20 21:11:02 -0300

    cleanup

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const app = express();
 
+// easier interact with mongodb
 const mongoose = require("mongoose");
+
+// handle our authentication
 const passport = require("passport");
+
+// creates the cookie session
 const session = require("express-session");
+
+// stores the session into the database and retrieve
 const MongoStore = require("connect-mongo")(session);
+
 const methodOverride = require("method-override");
+
 const flash = require("express-flash");
 const logger = require("morgan");
 
 const connectDB = require("./config/database");
+
+// routes
 const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/posts");
 const commentRoutes = require("./routes/comments");
@@ -30,7 +41,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Logging
+// Logging the request events that's happening
 app.use(logger("dev"));
 
 // Use forms for put / delete
@@ -62,3 +73,6 @@ app.use("/comment", commentRoutes);
 app.listen(process.env.PORT, () => {
   console.log(`✅ Server running at: http://localhost:${process.env.PORT}`);
 });
+
+// TODOS
+// Add new static files, like about us
